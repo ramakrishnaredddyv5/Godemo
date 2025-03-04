cf045f53cc2ce04e07f227986814ddfc64ef4db5
ramakrishnareddyv5 <ramakrishnareddyv505@gmail.com>
2025-03-04 16:46:45 +0530

    sample2

---
+++ go2.go
@@ -0,0 +1,13 @@
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+	fmt.Println("add go2 to github")
+	fmt.Println("git init")
+	fmt.Println("git add")
+	fmt.Println("git commit")
+	fmt.Println("git push")
+}
